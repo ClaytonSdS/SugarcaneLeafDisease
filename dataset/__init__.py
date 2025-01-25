819690efc324a581baa15c42f27c8b0fb0873a68
Clayton Silva dos Santos <95229093+ClaytonSdS@users.noreply.github.com>
2025-01-25 15:52:44 -0300

    Update __init__.py

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,16 +1,20 @@
-# SugarcaneLeafDisease/dataset/__init__.py
+import os
 from ..preprocessing.labeling.labeling_tools import ImageLabeling
-
 
 class LabeledDataset:
     def __init__(self):
+        base_dir = os.path.dirname(__file__) 
+        tfrecord_dir = os.path.join(base_dir, 'tfrecord_dataset') 
+        
+        tfrecord_files = [
+            os.path.join(tfrecord_dir, "tfrecord_dataset_part_1.tfrecord"),
+            os.path.join(tfrecord_dir, "tfrecord_dataset_part_2.tfrecord"),
+            os.path.join(tfrecord_dir, "tfrecord_dataset_part_3.tfrecord"),
+            os.path.join(tfrecord_dir, "tfrecord_dataset_part_4.tfrecord")
+        ]
+
         data_load = ImageLabeling()
-        data_load.load_data([
-            "tfrecord_dataset/tfrecord_dataset_part_1.tfrecord",
-            "tfrecord_dataset/tfrecord_dataset_part_2.tfrecord",
-            "tfrecord_dataset/tfrecord_dataset_part_3.tfrecord",
-            "tfrecord_dataset/tfrecord_dataset_part_4.tfrecord"
-        ])
+        data_load.load_data(tfrecord_files)
 
         self.parsed_dataset = data_load.parsed_dataset
 
