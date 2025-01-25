2a5b3de13823b7362adef8bfb5ef91ae57481585
Clayton Silva dos Santos <95229093+ClaytonSdS@users.noreply.github.com>
2025-01-25 15:42:16 -0300

    Update __init__.py

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,22 +1,18 @@
 # SugarcaneLeafDisease/dataset/__init__.py
-
-# Importação relativa
 from ..preprocessing.labeling.labeling_tools import ImageLabeling
 
 
 class LabeledDataset:
     def __init__(self):
-        # Carregando os dados a partir dos arquivos TFRecord
         data_load = ImageLabeling()
         data_load.load_data([
             "tfrecord_dataset/tfrecord_dataset_part_1.tfrecord",
             "tfrecord_dataset/tfrecord_dataset_part_2.tfrecord",
-            "tfrecord_dataset/tfrecord_dataset_part_3.tfrecord"
+            "tfrecord_dataset/tfrecord_dataset_part_3.tfrecord",
+            "tfrecord_dataset/tfrecord_dataset_part_4.tfrecord"
         ])
 
-        # Atribuindo a parsed_dataset à instância da classe
         self.parsed_dataset = data_load.parsed_dataset
 
     def __iter__(self):
-        # Permite iterar diretamente sobre o parsed_dataset
         return iter(self.parsed_dataset)
