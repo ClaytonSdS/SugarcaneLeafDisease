091b079334f9d46bbcb6bc9f6be7feaa63550208
Clayton Silva dos Santos <95229093+ClaytonSdS@users.noreply.github.com>
2025-03-19 13:22:35 -0300

    refactor: Restructure Model class and add new features
    - Updated the `__init__` method of the `Model` class to allow loading batch-based and patch-based prediction models.
    - Added `load_model` method to load models with support for custom objects such as `CBAM`, `ResidualBlock1`, and `ResidualBlock2`.
    - Updated `rescale_by_upscaling` method to use `target_size` for resizing images, with dimension checks.
    - Modified `predict` method to support prediction based on patches or batch processing depending on configuration.
    - Restructured image validation in `size_error_check` method to adjust the size as needed.
    - Fixed image shape validation in `shape_mismatch_error_check` method to ensure images are in `(batch, height, width, channels)` format.
    - Added `decode` functionality to convert predictions from class indices to human-readable labels.
    - Added `Model_18` class to load the specific model with predefined paths.
    - Removed redundant code and reorganized file structure for better readability.
    
    These changes improve the modularity of the code and make it easier to customize the model based on input and output requirements.

--- models.py
+++ models.py
@@ -3,7 +3,6 @@ import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow.keras.saving import register_keras_serializable
 from .custom_layers import CBAM, ResidualBlock1, ResidualBlock2
-import gdown
 import numpy as np
 import cv2
 from skimage.transform import resize
@@ -11,213 +10,187 @@ import jax
 import jax.numpy as jnp
 
 
+def calculate_padding(size:int, goal:int):
+  if size < goal:
+      pad = goal - size
+      if pad % 2 == 0: return pad // 2, pad // 2
+      else: return pad // 2 + 1, pad // 2
+
+  else:
+      return 0, 0
+
+
 class Model:
-    def __init__(self, filepath=None, patch=None):
+    def __init__(self, filepath: str = None, patch: str = None):
+        """
+        Initializes the Model class by loading the Keras models for batch-based and patch-based predictions.
+
+        Args:
+            filepath (str, optional): The file path to the Keras model for batch-based prediction. 
+                                      If None, the model is not loaded. Default is None.
+            patch (str, optional): The file path to the Keras model for patch-based prediction. 
+                                  If None, the patch model is not loaded. Default is None.
+
+        Initializes the `model_patch` and `model` attributes with the respective Keras models 
+        if the corresponding file paths are provided. The models are loaded using the 
+        `load_model` function, which also supports custom objects like `CBAM`, `ResidualBlock1`, and `ResidualBlock2`.
+
+        Example:
+            >>> model = Model(filepath="SugarcaneLeafDisease/model18.keras", patch="SugarcaneLeafDisease/model18_patches.keras")
+            >>> print(model.model)        # Batch-based model
+            >>> print(model.model_patch)  # Patch-based model
+        """
         self.filepath = filepath
+        self.model_patch = self.load_model(path=patch)
+        self.model = self.load_model(path=self.filepath,
+                                    custom_objects={'CBAM': CBAM, 'ResidualBlock1': ResidualBlock1, 'ResidualBlock2': ResidualBlock2})
 
-        self.model_patch = self.load_model(path = patch)
-        self.model = self.load_model(path = self.filepath, 
-                                     custom_objects={'CBAM': CBAM, 'ResidualBlock1': ResidualBlock1, 'ResidualBlock2': ResidualBlock2})
 
-    def load_model(self, path:str, custom_objects:dict=None):
+    def load_model(self, path: str, custom_objects: dict = None):
         return tf.keras.models.load_model(path, custom_objects)
 
-    #############################################################################################################################################################################
 
-    def rescale_by_upscaling(self, height_width:tuple, height_width_goal:tuple):
-        height, width = height_width
-        height_goal, width_goal = height_width_goal
+    def rescale_by_upscaling(self, array:np.array):
+      # using patches
+      if self.use_patches:
+        array_flatten = tf.reshape(array, [self.batch_size*self.rows*self.cols, self.patch_size, self.patch_size, 3]) 
+        print(f"[INFO] Reshaping patches (batch, rows, cols, pixels) → (b*r*c, patch_size, patch_size, channels): {array.shape} → {array_flatten.shape}")
 
-        scale_factor = min(height_goal / height, width_goal / width)
+        resized = tf.image.resize(array_flatten, self.target_size, preserve_aspect_ratio=True)        
+        print(f"[INFO] Upscaling patches to match model input shape {self.target_size}: {array_flatten.shape} → {resized.shape}")
 
-        # Calculating the new width and height, based on the scale factor got previously
-        new_width = int(width * scale_factor)
-        new_height = int(height * scale_factor)
+      # not using patches
+      else:
+        resized = tf.image.resize(array, self.target_size, preserve_aspect_ratio=True)        
+      
+      _, self.height, self.width, _ = resized.shape
+      return resized
 
-        # Rezing the image with the new (new_width, new_height) shape
-        resized_image = np.array([resize(img, (new_height, new_width), anti_aliasing=True) for img in self.array2predict])
-        return resized_image
-
-    def calculate_padding(self, size, goal):
-        """Calculate padding for a single dimension (height or width)"""
-        if size < goal:
-            pad = goal - size
-            # Return how much padding to apply on both sides (left and right, or top and bottom)
-            if pad % 2 == 0:
-                return pad // 2, pad // 2
-            else:
-                return pad // 2 + 1, pad // 2  # Slightly larger padding on the right or bottom side
-        else:
-            return 0, 0  # No padding required
-
-    def rescale_by_padding(self, height_width:tuple, height_width_goal:tuple):
-        """Resize the image by padding to the required size."""
-        height, width = height_width
-        height_goal, width_goal = height_width_goal
-
-        pad_height_left, pad_height_right = self.calculate_padding(height, height_goal)
-        pad_width_left, pad_width_right = self.calculate_padding(width, width_goal)
-
-        if pad_height_left > 0 or pad_width_left > 0:
-            # Apply padding if necessary
-            new_height = height + pad_height_left + pad_height_right
-            new_width = width + pad_width_left + pad_width_right
-
-            # Inform the user about the new shape after padding
-            print(f"Expected an image-size of at least ({height_goal}, {width_goal}), "
-                f"so, your image was resized via zero-padding to shape ({new_height}, {new_width}). "
-                f"Padding applied: height={pad_height_left+pad_height_right}, width={pad_width_left+pad_width_right}")
-            
-            return np.pad(self.array2predict,
-                        ((0, 0),  # No padding for batch dimension
-                        (pad_height_left, pad_height_right),  # Padding for height
-                        (pad_width_left, pad_width_right),  # Padding for width
-                        (0, 0)),  # No padding for channels
-                        mode='constant', constant_values=0)
-        else:
-            return self.array2predict
+    def size_error_check(self):
+        if self.height != self.target_size[0] or self.width != self.target_size[1]:
+            self.array2predict = self.rescale_by_upscaling(array=self.array2predict)  
 
 
-    #############################################################################################################################################################################
+    def shape_mismatch_error_check(self, data:np.array):
+        self.array2predict = np.array(data)  
+        shape = self.array2predict.shape    
 
-    def size_error_check(self, predicting_in_patches=False):
-        size2batches = 212
-        size2patches = 64
-
-        batch, height, width, channels = self.array2predict.shape  # input already in the format (batch, height, width, channels)
-
-
-        # not predicting in patches
-        if not predicting_in_patches:
-            if height != size2batches or width != size2batches:
-                self.array2predict = self.rescale_by_upscaling(height_width = (height, width), height_width_goal=(size2batches, size2batches))   # rescaling the images if necessary — either upscaling it or downscaling it.
-                self.array2predict = self.rescale_by_padding(height_width = (height, width), height_width_goal=(size2batches, size2batches))     # adding padding if necessary
-                
-        # predicting in patches:
-        else:
-            if height < size2patches or width < size2patches:
-                self.array2predict = self.rescale_by_upscaling(height_width = (height, width), height_width_goal=(size2batches, size2batches))   # upscaling the images if necessary.
-                self.array2predict = self.rescale_by_padding(height_width = (height, width), height_width_goal=(size2batches, size2batches))     # adding padding if necessary
-
-
-    def shape_mismatch_error_check(self, data):
-        """
-        Check if the input data shape matches the expected format.
-
-        Parameters:
-        - data (numpy array or list): The input image or batch of images.
-        
-        Raises:
-        - ValueError: If the input shape is invalid or the number of channels is not 3.
-        """
-        self.array2predict = np.array(data)  # Convert to NumPy array if not already
-        shape = self.array2predict.shape  # Get the shape of the data
-
-        # Mismatch checking: the model expects input in the format (batch, height, width, channels)
         if len(shape) != 4:
-            if len(shape) == 3:  # Case of a single image (height, width, channels)
-                self.array2predict = np.expand_dims(self.array2predict, axis=0)  # Add batch dimension
-                
-                # Verify the number of channels
-                num_channels = self.array2predict.shape[-1]  # Getting the number of channels from the new expanded image
-                
-                # Check if the channels are different than 3
+            if len(shape) == 3:  
+                self.array2predict = np.expand_dims(self.array2predict, axis=0)  
+                num_channels = self.array2predict.shape[-1]  
                 if num_channels != 3:
-                    raise ValueError(f"Expected images with 3 channels, but got {num_channels} channel(s)")
+                    raise ValueError(f"Expected 3 channels, but received {num_channels}")
             else:
-                raise ValueError(f"Expected shape (batch, height, width, channels), but got {shape}")
+                raise ValueError(f"Expected shape (batch, height, width, channels), but received {shape}")
+                
+        self.batch_size, self.height, self.width, self.channels = self.array2predict.shape 
+
+    def patches_check_validation(self):
+        if self.patch_size > min(self.height, self.width):
+            raise ValueError(f"The maximum patch size for this configuration is {min(self.height, self.width)}")
+      
+        elif self.patch_size <= 0:
+          raise ValueError(f"The minimum patch size must be 1")
 
 
-    #############################################################################################################################################################################
-
-
-    def extract_patches_jax(self, images, patch_size=64):
-        batch, height, width, channels = images.shape
-
-        # Calcula o número de patches na altura e na largura
-        n_patches_h = height // patch_size
-        n_patches_w = width // patch_size
-        n_patches = n_patches_h * n_patches_w  # Número total de patches por imagem
-
-        # Índices para a extração dinâmica dos patches
-        i_idx = jnp.arange(n_patches_h) * patch_size    # i.e. [0,1,2,3] becomes [0, 64, 128, 256]
-        j_idx = jnp.arange(n_patches_w) * patch_size    # i.e. [0,1,2,3] becomes [0, 64, 128, 256]
-
-        # Cria uma grade de índices
-        i_idx, j_idx = jnp.meshgrid(i_idx, j_idx, indexing='ij')  # Grid with all combinations of i and j indexes: (n_patches_h, n_patches_w)
-        i_idx = i_idx.flatten()
-        j_idx = j_idx.flatten()
-
-        def extract_single_image(img):
-            """extract patches of a single image using JAX dynamic_slice"""
-            patches = jax.vmap(lambda i, j: jax.lax.dynamic_slice(img, (i, j, 0), (patch_size, patch_size, channels)))(i_idx, j_idx)
-            return patches  # (n_patches, 64, 64, 3)
-
-        # Aplica a extração a todas as imagens do batch
-        patches_batch = jax.vmap(extract_single_image)(images)  # (batch, n_patches, 64, 64, 3)
-        
-        return patches_batch
-
-    #############################################################################################################################################################################
-    
-
-    def predict_in_paches(self, images):
-        self.shape_mismatch_error_check(images) #  Check if the shape of the input data is correct
-        self.size_error_check()                 #  Checking if image-size have a proper minimum size
-
-        patches = self.extract_patches_jax(jnp.array(self.array2predict))  # (batch, n_patches, 64, 64, 3)
-
-        # Realiza a predição em todos os patches
-        predicted_patches = jax.vmap(self.model_patch.predict)(patches)
-
-        # Converte para classes
-        self.predicted_classes = jnp.argmax(predicted_patches, axis=-1)
-
-        self.last_predict_form = "in_patches"
-        return self.predicted_classes
-
-
-    def predict_in_batches(self, images):
+    def predict(self, images: np.array, patch_size: int = 64, use_patches: bool = True, use_patch_model: bool = False):
         """
-        Perform batch predictions on input images.
+        Performs prediction on the input images using either a patch-based or batch-based approach.
 
-        Parameters:
-        - images (numpy array or list): The input images, expected to be in one of the following formats:
-          1. (batch, height, width, 3) → Directly usable in model prediction.
-          2. (height, width, 3) → A single image; it will be expanded to (1, height, width, 3).
+        Args:
+            images (np.ndarray): Input images with shape (batch, height, width, channels).
+            patch_size (int, optional): The size of the patches to extract when using patch-based prediction. Default is 64.
+            use_patches (bool, optional): If True, the images will be divided into patches before prediction. Default is True.
+            use_patch_model (bool, optional): If True, uses the patch-based model instead of the full-image model. Default is False.
 
         Returns:
-        - numpy array: Predicted class indices for each input image.
-        
-        Raises:
-        - ValueError: If the input shape is invalid or the number of channels is not 3.
+            tf.Tensor: Predicted output with shape (batch, rows, cols, 5) when using patches,  
+                      where `rows` and `cols` define the grid layout of patches extracted from each image.
+                      e.g., predict[0] shows the all the grids of image array in batch=0.  
+                      In batch-based prediction, the output shape is (batch, 1, 1, 5), representing a single prediction per image.  
+                      The last dimension corresponds to classification scores for 5 categories.
         """
 
-        self.shape_mismatch_error_check(images) #  Check if the shape of the input data is correct
-        self.size_error_check()                 #  Checking if image-size have a proper minimum size
-        
-        self.predicted = self.model.predict(self.array2predict)
-        self.predicted_classes = np.argmax(self.predicted, axis=1) 
+        images = np.array(images)
+        self.use_patches = use_patches
+        self.patch_size = patch_size
+        self.use_patch_model = use_patch_model
 
-        self.last_predict_form = "in_batches"
-        return self.predicted_classes
-    
-    #############################################################################################################################################################################
+        # Check to disable patch model if not using patches
+        if not self.use_patches:
+            self.use_patch_model = False
+            print("[INFO] Not using patches, so use_patch_model set to False")
 
-    def convert_predictions_to_labels(self):
-        """
-        Decode the model's predicted class indices into labels.
+        self.target_size = ((212, 212) if not self.use_patch_model or not self.use_patches else (64, 64))  
 
-        This function maps the model's output class indices (0, 1, 2, 3, 4) to 
-        corresponding  (e.g., 'healthy', 'mosaic', 'yellow', 'redrot', 'rust'), 
-        and prints the decoded labels.
+        print(f'[INFO] Target size set to {self.target_size}')
 
-        Returns:
-        - list: A list of predicted class labels in string format (e.g., ['healthy', 'mosaic', ...]).
-        """
-        labels_inversed = {0:'healthy', 1:'mosaic', 2:'yellow', 3:'redrot', 4:'rust'}
-        self.predicted_decoded = [labels_inversed[label] for label in self.predicted_classes]
-        return self.predicted_decoded
+        self.shape_mismatch_error_check(images)
+        print("[INFO] Shape validation: completed successfully")
+
+        self.patches_check_validation()
+        print("[INFO] Patch validation: completed successfully")
+
+        if self.use_patches:
+            self.patches = tf.image.extract_patches(images=self.array2predict, sizes=[1, patch_size, patch_size, 1],
+                                                    strides=[1, patch_size, patch_size, 1],
+                                                    rates=[1, 1, 1, 1], padding="VALID")      
+            self.rows, self.cols = self.patches.shape[1:3]                                    
+            self.array2predict = self.patches
+
+            self.height, self.width = (self.patch_size, self.patch_size)
+
+            print(f"[INFO] Patch extraction completed - Output shape: {self.array2predict.shape}, Batch size: {self.batch_size}, Grid shape: ({self.rows},{self.cols}) = {self.rows*self.cols} patches, Patch size: ({self.patch_size},{self.patch_size}, {self.channels}) = {self.channels*self.patch_size**2} pixels")
+
+        self.size_error_check()
+        print("[INFO] Image size validation: completed successfully")
+
+        if self.use_patches:  
+            model_to_use = self.model_patch if self.use_patch_model else self.model   # selecting the proper model to predict in patches
+            self.predicted = model_to_use.predict(self.array2predict)  
+            self.predicted = tf.reshape(self.predicted, [self.batch_size, self.rows, self.cols, 5])  
+            print(f"[INFO] Patch-based prediction completed - Output shape: {self.predicted.shape}")  
+
+        else:  
+            print("[INFO] Using batch-based model for prediction")  
+            print(f"[INFO] Input shape set to {self.target_size}")  
+            self.predicted = self.model.predict(self.array2predict)  
+            self.predicted = tf.reshape(self.predicted, [self.batch_size, 1, 1, 5])  
+            print(f"[INFO] Batch-based prediction completed - Output shape: {self.predicted.shape}")  
+
+        return self.predicted
+
+
+
+    def decode(self, pred: tf.Tensor = None):
+      """
+      Decodes the model's prediction tensor into class labels.
+
+      Args:
+          pred (tf.Tensor, optional): A tensor containing the model's predictions. If None, 
+                                      the function uses `self.predicted`. Default is None.
+
+      Returns:
+          np.ndarray: An array containing the predicted class labels as strings.
+
+      Example:
+          >>> model = Model()
+          >>> predictions = model.predict(images)
+          >>> decoded_labels = model.decode(predictions)
+          >>> print(decoded_labels)
+          [['healthy' 'mosaic' 'yellow']
+          ['redrot' 'rust' 'healthy']]
+      """
+      labels_inversed = {0: 'healthy', 1: 'mosaic', 2: 'yellow', 3: 'redrot', 4: 'rust'}
+
+      pred = pred if pred is not None else self.predicted
+
+      return np.vectorize(labels_inversed.get)(
+          tf.argmax(pred, axis=-1)
+      )
+
+
 
 
 
