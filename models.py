ea5ddde6d8bfb9ae7e5fb1de13243aa79216adc1
Clayton Silva dos Santos <95229093+ClaytonSdS@users.noreply.github.com>
2025-03-19 14:31:39 -0300

    fix: refactor rescale_by_upscaling() and predict() to resolve bug found.
    - Refactored rescale_by_upscaling() for a simplified resizing pipeline.
    - Fixed bug in predict() related to patch extraction shape.
    - Improved pipeline through logs for better tracking.
    - Introduced verbose parameter to control logging output.

--- models.py
+++ models.py
@@ -20,61 +20,45 @@ def calculate_padding(size:int, goal:int):
       return 0, 0
 
 
+
 class Model:
-    def __init__(self, filepath: str = None, patch: str = None):
+    def __init__(self, filepath: str = None, patch: str = None, verbose: bool = True):
         """
-        Initializes the Model class by loading the Keras models for batch-based and patch-based predictions.
+        Inicializa a classe Model, carregando os modelos Keras para previsões em lote e baseadas em patch.
 
         Args:
-            filepath (str, optional): The file path to the Keras model for batch-based prediction. 
-                                      If None, the model is not loaded. Default is None.
-            patch (str, optional): The file path to the Keras model for patch-based prediction. 
-                                  If None, the patch model is not loaded. Default is None.
-
-        Initializes the `model_patch` and `model` attributes with the respective Keras models 
-        if the corresponding file paths are provided. The models are loaded using the 
-        `load_model` function, which also supports custom objects like `CBAM`, `ResidualBlock1`, and `ResidualBlock2`.
-
-        Example:
-            >>> model = Model(filepath="SugarcaneLeafDisease/model18.keras", patch="SugarcaneLeafDisease/model18_patches.keras")
-            >>> print(model.model)        # Batch-based model
-            >>> print(model.model_patch)  # Patch-based model
+            filepath (str, opcional): Caminho para o modelo Keras de previsão em lote.
+            patch (str, opcional): Caminho para o modelo Keras de previsão baseada em patch.
+            verbose (bool, opcional): Se True, habilita a exibição de logs no terminal. Padrão é True.
         """
+        self.verbose = verbose
         self.filepath = filepath
         self.model_patch = self.load_model(path=patch)
         self.model = self.load_model(path=self.filepath,
-                                    custom_objects={'CBAM': CBAM, 'ResidualBlock1': ResidualBlock1, 'ResidualBlock2': ResidualBlock2})
+                                     custom_objects={'CBAM': CBAM, 'ResidualBlock1': ResidualBlock1, 'ResidualBlock2': ResidualBlock2})
 
+    def print_verbose(self, message: str):
+        if self.verbose:
+            print(message)
 
     def load_model(self, path: str, custom_objects: dict = None):
         return tf.keras.models.load_model(path, custom_objects)
 
-
-    def rescale_by_upscaling(self, array:np.array):
-      # using patches
-      if self.use_patches:
-        array_flatten = tf.reshape(array, [self.batch_size*self.rows*self.cols, self.patch_size, self.patch_size, 3]) 
-        print(f"[INFO] Reshaping patches (batch, rows, cols, pixels) → (b*r*c, patch_size, patch_size, channels): {array.shape} → {array_flatten.shape}")
-
-        resized = tf.image.resize(array_flatten, self.target_size, preserve_aspect_ratio=True)        
-        print(f"[INFO] Upscaling patches to match model input shape {self.target_size}: {array_flatten.shape} → {resized.shape}")
-
-      # not using patches
-      else:
-        resized = tf.image.resize(array, self.target_size, preserve_aspect_ratio=True)        
-      
-      _, self.height, self.width, _ = resized.shape
-      return resized
+    def rescale_by_upscaling(self, array: np.array):
+        resized = tf.image.resize(array, self.target_size, preserve_aspect_ratio=True)    
+        self.print_verbose(f"[INFO] Upscaling patches to match model input shape {self.target_size}: {array.shape} → {resized.shape}")
+        _, self.height, self.width, _ = resized.shape
+        return resized
 
     def size_error_check(self):
         if self.height != self.target_size[0] or self.width != self.target_size[1]:
             self.array2predict = self.rescale_by_upscaling(array=self.array2predict)  
+        else:
+            self.print_verbose(f"[INFO] No need for upscaling. The current input shape is already {self.array2predict.shape}, which matches the target size {self.target_size}.")
 
-
-    def shape_mismatch_error_check(self, data:np.array):
+    def shape_mismatch_error_check(self, data: np.array):
         self.array2predict = np.array(data)  
         shape = self.array2predict.shape    
-
         if len(shape) != 4:
             if len(shape) == 3:  
                 self.array2predict = np.expand_dims(self.array2predict, axis=0)  
@@ -89,114 +73,108 @@ class Model:
     def patches_check_validation(self):
         if self.patch_size > min(self.height, self.width):
             raise ValueError(f"The maximum patch size for this configuration is {min(self.height, self.width)}")
-      
         elif self.patch_size <= 0:
-          raise ValueError(f"The minimum patch size must be 1")
-
+            raise ValueError(f"The minimum patch size must be 1")
 
     def predict(self, images: np.array, patch_size: int = 64, use_patches: bool = True, use_patch_model: bool = False):
-        """
-        Performs prediction on the input images using either a patch-based or batch-based approach.
-
-        Args:
-            images (np.ndarray): Input images with shape (batch, height, width, channels).
-            patch_size (int, optional): The size of the patches to extract when using patch-based prediction. Default is 64.
-            use_patches (bool, optional): If True, the images will be divided into patches before prediction. Default is True.
-            use_patch_model (bool, optional): If True, uses the patch-based model instead of the full-image model. Default is False.
-
-        Returns:
-            tf.Tensor: Predicted output with shape (batch, rows, cols, 5) when using patches,  
-                      where `rows` and `cols` define the grid layout of patches extracted from each image.
-                      e.g., predict[0] shows the all the grids of image array in batch=0.  
-                      In batch-based prediction, the output shape is (batch, 1, 1, 5), representing a single prediction per image.  
-                      The last dimension corresponds to classification scores for 5 categories.
-        """
-
         images = np.array(images)
         self.use_patches = use_patches
         self.patch_size = patch_size
         self.use_patch_model = use_patch_model
 
-        # Check to disable patch model if not using patches
         if not self.use_patches:
             self.use_patch_model = False
-            print("[INFO] Not using patches, so use_patch_model set to False")
+            self.print_verbose("[INFO] Not using patches, so use_patch_model set to False")
 
         self.target_size = ((212, 212) if not self.use_patch_model or not self.use_patches else (64, 64))  
-
-        print(f'[INFO] Target size set to {self.target_size}')
+        self.print_verbose(f'[INFO] Target size set to {self.target_size}')
 
         self.shape_mismatch_error_check(images)
-        print("[INFO] Shape validation: completed successfully")
+        self.print_verbose("[INFO] Shape validation: completed successfully")
 
         self.patches_check_validation()
-        print("[INFO] Patch validation: completed successfully")
+        self.print_verbose("[INFO] Patch validation: completed successfully")
 
         if self.use_patches:
             self.patches = tf.image.extract_patches(images=self.array2predict, sizes=[1, patch_size, patch_size, 1],
                                                     strides=[1, patch_size, patch_size, 1],
-                                                    rates=[1, 1, 1, 1], padding="VALID")      
+                                                    rates=[1, 1, 1, 1], padding="VALID")     
             self.rows, self.cols = self.patches.shape[1:3]                                    
             self.array2predict = self.patches
+            self.print_verbose(f"[INFO] Patch extraction completed - Output shape: {self.array2predict.shape}, Batch size: {self.batch_size}, Grid shape: ({self.rows},{self.cols}) = {self.rows*self.cols} patches, Patch size: ({self.patch_size},{self.patch_size}, {self.channels}) = {self.channels*self.patch_size**2} pixels")
+
+            self.array2predict = tf.reshape(self.array2predict, [self.batch_size*self.rows*self.cols, self.patch_size, self.patch_size, 3]) 
+            self.print_verbose(f"[INFO] Reshaping patches (batch, rows, cols, pixels) → (b*r*c, patch_size, patch_size, channels): {self.patches.shape} → {self.array2predict.shape}")
 
             self.height, self.width = (self.patch_size, self.patch_size)
 
-            print(f"[INFO] Patch extraction completed - Output shape: {self.array2predict.shape}, Batch size: {self.batch_size}, Grid shape: ({self.rows},{self.cols}) = {self.rows*self.cols} patches, Patch size: ({self.patch_size},{self.patch_size}, {self.channels}) = {self.channels*self.patch_size**2} pixels")
-
         self.size_error_check()
-        print("[INFO] Image size validation: completed successfully")
+        self.print_verbose("[INFO] Image size validation: completed successfully")
 
         if self.use_patches:  
-            model_to_use = self.model_patch if self.use_patch_model else self.model   # selecting the proper model to predict in patches
+            model_to_use = self.model_patch if self.use_patch_model else self.model   
             self.predicted = model_to_use.predict(self.array2predict)  
             self.predicted = tf.reshape(self.predicted, [self.batch_size, self.rows, self.cols, 5])  
-            print(f"[INFO] Patch-based prediction completed - Output shape: {self.predicted.shape}")  
-
+            self.print_verbose(f"[INFO] Patch-based prediction completed - Output shape: {self.predicted.shape}")  
         else:  
-            print("[INFO] Using batch-based model for prediction")  
-            print(f"[INFO] Input shape set to {self.target_size}")  
+            self.print_verbose("[INFO] Using batch-based model for prediction")  
+            self.print_verbose(f"[INFO] Input shape set to {self.target_size}")  
             self.predicted = self.model.predict(self.array2predict)  
             self.predicted = tf.reshape(self.predicted, [self.batch_size, 1, 1, 5])  
-            print(f"[INFO] Batch-based prediction completed - Output shape: {self.predicted.shape}")  
+            self.print_verbose(f"[INFO] Batch-based prediction completed - Output shape: {self.predicted.shape}")  
 
         return self.predicted
 
-
-
     def decode(self, pred: tf.Tensor = None):
-      """
-      Decodes the model's prediction tensor into class labels.
+        """
+        Decodes the model's prediction tensor into class labels.
 
-      Args:
-          pred (tf.Tensor, optional): A tensor containing the model's predictions. If None, 
-                                      the function uses `self.predicted`. Default is None.
+        Args:
+            pred (tf.Tensor, optional): A tensor containing the model's predictions. If None, 
+                                        the function uses `self.predicted`. Default is None.
 
-      Returns:
-          np.ndarray: An array containing the predicted class labels as strings.
+        Returns:
+            np.ndarray: An array containing the predicted class labels as strings.
 
-      Example:
-          >>> model = Model()
-          >>> predictions = model.predict(images)
-          >>> decoded_labels = model.decode(predictions)
-          >>> print(decoded_labels)
-          [['healthy' 'mosaic' 'yellow']
-          ['redrot' 'rust' 'healthy']]
-      """
-      labels_inversed = {0: 'healthy', 1: 'mosaic', 2: 'yellow', 3: 'redrot', 4: 'rust'}
+        Example:
+            >>> model = Model()
+            >>> predictions = model.predict(images)
+            >>> decoded_labels = model.decode(predictions)
+            >>> print(decoded_labels)
+            [['healthy' 'mosaic' 'yellow']
+            ['redrot' 'rust' 'healthy']]
+        """
+        labels_inversed = {0: 'healthy', 1: 'mosaic', 2: 'yellow', 3: 'redrot', 4: 'rust'}
 
-      pred = pred if pred is not None else self.predicted
+        pred = pred if pred is not None else self.predicted
 
-      return np.vectorize(labels_inversed.get)(
-          tf.argmax(pred, axis=-1)
-      )
-
-
+        return np.vectorize(labels_inversed.get)(tf.argmax(pred, axis=-1))
 
 
 
 class Model_18(Model):
-    def __init__(self):
+    """
+    A subclass of the `Model` class, specifically designed for loading and predicting with the model 18 architecture.
+
+    Args:
+        verbose (bool, optional): If True, enables verbose logging throughout the model's operations, providing detailed information 
+                                  about each step. Default is True.
+
+    Inherits:
+        Model: A parent class responsible for handling the base operations of model loading, prediction, and image processing.
+
+    Example:
+        >>> model = Model_18(verbose=True)
+        >>> predictions = model.predict(images)
+    
+    Attributes:
+        verbose (bool): The verbosity level for logging information during model operations.
+        model (tf.keras.Model): The Keras model for batch-based predictions.
+        model_patch (tf.keras.Model): The Keras model for patch-based predictions.
+    """
+    
+    def __init__(self, verbose: bool=True):
         dir_path = os.path.dirname(os.path.abspath(__file__)) 
         path = os.path.join(dir_path, "model18.keras")  
         patch = os.path.join(dir_path, "model18_patches.keras")  
-        super().__init__(filepath=path, patch=patch)
+        super().__init__(filepath=path, patch=patch, verbose=verbose)
